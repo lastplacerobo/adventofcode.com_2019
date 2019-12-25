cfd8e1e588493c5a2ed5ffe00797dbdd39c93bca
lastplacerobo <robert.bothin@gmail.com>
2019-12-25 21:29:08 +0100

    .

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -1,5 +1,11 @@
 import os
 
+# Constants, cmd+alt+c, cmd,alt,v for variables
+# Use refactor for name change of variable
+FINISH = 99
+MULTIPLY = 2
+ADDITION = 1
+
 
 def main():
     # Get current dir and append file name, so that the script can be run anywhere as long as the input file is in the
@@ -10,50 +16,66 @@ def main():
     file_input = open(file_path, 'r')
 
     # Read line into list and split the string into each index
-    intcode_list = (file_input.read())
+    intcodes = (file_input.read())
 
-    intcode_list = intcode_list.split(',')
+    intcodes = intcodes.split(',')
 
     # Convert string list into int
-    intcode_list = [int(x) for x in intcode_list]
+    intcodes = [int(x) for x in intcodes]
 
-    for noun in range((len(intcode_list))):
+    # you need to determine what pair of inputs produces the output 19690720.
+    # The inputs should still be provided to the program by replacing the values
+    # at addresses 1 and 2, just like before. In this program, the value placed
+    # in address 1 is called the noun, and the value placed in address 2 is called the verb.
+    # Each of the two input values will be between 0 and 99, inclusive.
 
-        for verb in range((len(intcode_list))):
+    # Run trough noun and verb up to
+    for noun in range(100):
 
-            shallow_intcode_list = intcode_list.copy()
+        # Trying with taking the length of the list (int) and making a range of it.
+        # Cannot just run len, since it just produces an int, and "for in" requires a list or range
+        for verb in range((len(intcodes))):
+
+            # Make a shallow copy of the original list to work with
+            mutable_intcodes = intcodes.copy()
 
 
             # Loop through the list with step 4 to get the opcode
-            for i in range(0,len(shallow_intcode_list),4):
+            for i in range(0,len(mutable_intcodes),4):
 
-                shallow_intcode_list[1] = noun
-                shallow_intcode_list[2] = verb
+                mutable_intcodes[1] = noun
+                mutable_intcodes[2] = verb
 
                 # Opcode 1 adds together numbers read from two positions and stores the result in a third position.
                 # The three integers immediately after the opcode tell you these three positions - the first two
                 # indicate the positions from which you should read the input values, and the third indicates
                 # the position at which the output should be stored.
-                if shallow_intcode_list[i] == 1:
+                opcode = mutable_intcodes[i]
+                pos_store = mutable_intcodes[i + 3]
+                read_pos1 = mutable_intcodes[i + 1]
+                read_pos2 = mutable_intcodes[i + 2]
 
-                    shallow_intcode_list[shallow_intcode_list[i+3]] = shallow_intcode_list[shallow_intcode_list[i+1]] \
-                                                                       + shallow_intcode_list[shallow_intcode_list[i+2]]
+                if opcode == ADDITION:
+
+                    mutable_intcodes[pos_store] = mutable_intcodes[read_pos1] + mutable_intcodes[read_pos2]
 
                 # Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them.
-                # Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
-                elif shallow_intcode_list[i] == 2:
+                # Again, the three integers after the opcode indicate where the inputs and outputs are, not their values
+                elif opcode == MULTIPLY:
 
-                    shallow_intcode_list[shallow_intcode_list[i + 3]] = shallow_intcode_list[shallow_intcode_list[i + 1]] \
-                                                                        * shallow_intcode_list[shallow_intcode_list[i + 2]]
+                    mutable_intcodes[pos_store] = mutable_intcodes[read_pos1] * mutable_intcodes[read_pos2]
 
-                elif shallow_intcode_list[i] == 99:
+                # Opcode 99 means that the program is finished and should immediately halt
+                elif opcode == FINISH:
                     break
 
                 else:
-                    raise Exception('Opcode should be 1/2/99. The Opcode value was:{}'.format(intcode_list[i]))
+                    raise Exception('Opcode should be 1/2/99. The Opcode value was:{}'.format(intcodes[i]))
 
-            if shallow_intcode_list[0] == 19690720:
+            # If we find our value print it and exit
+            if mutable_intcodes[0] == 19690720:
                 print(100*noun+verb)
+                exit()
 
 
 
