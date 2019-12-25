9b993f347eacb523fe28468dcdd804a2979dc686
lastplacerobo <robert.bothin@gmail.com>
2019-12-25 16:32:24 +0100

    .

--- day2/day2.py
+++ day2/day2.py
@@ -17,6 +17,39 @@ def main():
     # Convert string list into int
     intcode_list = [int(x) for x in intcode_list]
 
+    # Replace position 1 with the value 12 and replace position 2 with the value 2.
+    intcode_list[1] = 12
+    intcode_list[2] = 2
+
+    # Loop through the list with step 4 to get the opcode
+    for i in range(0,len(intcode_list),4):
+
+        # Opcode 1 adds together numbers read from two positions and stores the result in a third position.
+        # The three integers immediately after the opcode tell you these three positions - the first two
+        # indicate the positions from which you should read the input values, and the third indicates
+        # the position at which the output should be stored.
+        if intcode_list[i] == 1:
+
+            intcode_list[intcode_list[i+3]] = intcode_list[intcode_list[i+1]] + intcode_list[intcode_list[i+2]]
+
+        # Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them.
+        # Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
+        elif intcode_list[i] == 2:
+
+            intcode_list[intcode_list[i + 3]] = intcode_list[intcode_list[i + 1]] * intcode_list[intcode_list[i + 2]]
+
+        elif intcode_list[i] == 99:
+            break
+
+        else:
+            raise Exception('Opcode should be 1/2/99. The Opcode valie was:{}'.format(intcode_list[i]))
+
+
+    # The value that is left at position 0 after the program halts
+    print(intcode_list[0])
+
+
+
 
 # Only run main if executed directly
 if __name__ == "__main__":
