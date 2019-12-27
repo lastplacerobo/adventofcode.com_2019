c613f4628f4547bf4b4753d5478ca1abb7c4cd29
lastplacerobo <robert.bothin@gmail.com>
2019-12-27 23:56:05 +0100

    .

--- day3/day3_part1.py
+++ day3/day3_part1.py
@@ -1,60 +1,57 @@
 import os
-import numpy
+
 
 # Returns the directory in which the code file is stored
 # Returns the absolute path, but does NOT resolve symlinks in its argument
-#os.path.dirname(os.path.abspath( __file__ ))
+# os.path.dirname(os.path.abspath( __file__ ))
 
 # Returns the directory in which the code file is stored
 # Will first resolve any symbolic links in the path, and then return the absolute path.
-#os.path.dirname(os.path.realpath(__file__))
+# os.path.dirname(os.path.realpath(__file__))
 
 # Returns the directory in which the code file is stored
-#os.path.dirname(__file__)
+# os.path.dirname(__file__)
 
 # Returns the full path to the code file
-#__file__
+# __file__
 
 # Defaults to $PWD in your env. It's not where the script is located but where you were when you executed the script.
-#os.getcwd()
+# os.getcwd()
 
 ############################
 
 def main():
-
     dir = os.path.dirname(__file__)
     # open and join the dir with the file input as f
     with open(os.path.join(dir, "input")) as f:
-
         # Read from f, strip spaces, split into list by ","
         # Map takes function str (could also be int) and reads whole list and converts to str as a iterator
         # List converts the argument into a list and stores str in mem
         # mem = list(map(str, f.read().strip().split(',')))
 
         # List comprehension iterating trough lines and split on comma, saving in two lists
-        wire_path1,wire_path2 = [i.split(',') for i in f.readlines()]
+        wire_path1, wire_path2 = [i.split(',') for i in f.readlines()]
 
         # Clean up newline in last element
         wire_path1[-1] = wire_path1[-1].strip()
         wire_path2[-1] = wire_path2[-1].strip()
 
-        grid_run(wire_path1)
+        grid_path1 = grid_run(wire_path1)
+        grid_path2 = grid_run(wire_path2)
 
-        #print("\n")
-        #print(grid_run(wire_path2))
+        distance_to_collision(grid_path1, grid_path2)
 
 
 def grid_run(route):
+    # Old list that was too slow
+    # List for the final route_path
+    #route_path = []
 
-    # Dictionary for the final route_path
-    route_path = {}
+    # Set for the final route_path
+    route_path = set()
 
-    # Variables for
-    x_axis, y_axis, count = 0, 0, 0
-
-    # With numpy create 2D array
-    grid1 = numpy.zeros((10000, 10000))
-    grid2 = numpy.zeros((10000, 10000))
+    # Variables for x/y axis
+    x_axis, y_axis = 0, 0
 
     # Movement in the X and Y axis
     movement = {
@@ -72,18 +69,47 @@ def grid_run(route):
         # after the letter in the element.
         # Taking the int and making a range of it, iterate trough this range
         for i in range((int(element[1:]))):
-
             # In this loop, we will track the steps for this specific route element
-            #
+            # i indicates each step needed to be taken in the grid on the x or y axis
 
+            # To get the direction, take the route element (R990)and filter out the first letter for direction
+            # R/L/U/D, take this letter as the key for the movement dictionary and save the value in offset (ex "1,0")
+            # Offset will then have a value such as [1, 0]
             offset = movement[element[0]]
+
+            # Take the offset ex [1, 0] and for the first element in the offset list save the value in the x axis.
+            # For the second element in the list save the value in the y axis
+            # Add the offset value so that the route moves trough the x and y axis grid, For route element R990 add
+            # 990 steps to the x-axis
             x_axis += offset[0]
             y_axis += offset[1]
-            count += 1
 
-            route_path[(x_axis, y_axis)] = count
+            # OLD LIST, was too slow
+            # In route path list append tuple of x and y coordinates for each step so we can track each single step
+            # that the wire takes in the grid. So for R990, first element is 1,0 next 2,0 next 3,0 and so on
+            # Last element in route_path list will have the final grid coordinate for the wire
+            #route_path.append((x_axis, y_axis))
 
+            route_path.add((x_axis,y_axis))
+
+    # Return the whole list with every single coordinate that the wire has taken
     return route_path
+
+def distance_to_collision(route1, route2):
+
+# Tried with a list, but to slow
+#    for i, r_i in enumerate(route1):
+
+#        for j, r_j in enumerate(route2):
+
+#            if r_i == r_j:
+#                print("collision")
+
+
+    collisions = route1 & route2
+
+    print(len(collisions))
+
 
 
 if __name__ == "__main__":
