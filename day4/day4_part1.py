ee0a3c11944e82bc746469b43a272191b2d31039
lastplacerobo <robert.bothin@gmail.com>
2020-07-04 20:07:07 +0200

    .

--- day4/day4_part1.py
+++ day4/day4_part1.py
@@ -4,7 +4,7 @@
 # Password in range(146810,612564)
 # Two adjacent digits are the same (like 22 in 122345).
 # Going from left to right, the digits never decrease;
-# they only ever increase or stay the same (like 111123 or 135679).
+# they only ever increase or stay the same (like 111123 or 135679)..
 
 # The following are true:
 #    111111 meets these criteria (double 11, never decreases).
